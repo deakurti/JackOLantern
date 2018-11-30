2e96e1d4dc3df94c76ff46ffcc447379f9dcf3e6
Dea Kurti <dkurti4965@bths.edu>
2018-11-30 01:25:58 -0500

    done

---
+++ src/JackOLantern.java
@@ -0,0 +1,27 @@
+public class JackOLantern {
+    private String[] [] faceFeatures;
+
+    public JackOLantern(String[] [] faceFeatures){
+        this.faceFeatures=faceFeatures;
+    }
+    public void edit(String replace,int row, int column){
+        faceFeatures[row][column] = replace;
+    }
+    public void fill(String str) {
+        for (int i = 0; i<faceFeatures.length; i++){
+            for(int j = 0; j<faceFeatures[i].length; j++){
+                faceFeatures[i][j] = str;
+            }
+        }
+    }
+    public String toString() {
+        String newface = ""; //I don't get this line. I asked two of my classmates because I was confused//
+        for(int i = 0; i<faceFeatures.length; i++){
+            for(int j = 0; j<faceFeatures[i].length; j++){
+                newface = (faceFeatures[i][j]);
+            }
+            newface+=("\n");
+        }
+        return newface;
+    }
+}
--- src/Runner.java
+++ src/Runner.java
@@ -1,18 +1,18 @@
 public class Runner {
     public static void main(String[] args) {
         String[][] face = new String[5][8];
-        JackoLantern face1 = new JackoLantern(face);
+        JackOLantern face1 = new JackOLantern(face);
 
         face1.fill("X");
-        face.edit(" ", 0, 0);
-        face.edit(" ", 4, 7);
-        face.edit(" ", 0, 7);
-        face.edit(" ", 4, 0);
-
-        face.edit("0", 1, 2);
-        face.edit("0", 1, 5);
-        face.edit("^", 2, 3);
-        face.edit("^", 2, 4);
-        face.edit(" ", 0, 0);
-    }
+        face1.edit("0", 1, 2);
+        face1.edit("0", 1, 5);
+        face1.edit("^", 2, 3);
+        face1.edit("^", 2, 4);
+        face1.edit("|", 3, 1);
+        face1.edit("|", 3, 2);
+        face1.edit("-", 3, 3);
+        face1.edit("-", 3, 4);
+        face1.edit("|", 3, 5);
+        face1.edit("|", 3, 6);
+        System.out.println(face1);
 }
