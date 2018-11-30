a511292ef90489bc477f2a5640f457f50c060552
Dea Kurti <dkurti4965@bths.edu>
2018-11-30 01:37:23 -0500

    done

--- src/Runner.java
+++ src/Runner.java
@@ -1,18 +1,15 @@
 public class Runner {
     public static void main(String[] args) {
-        String[][] face = new String[5][8];
+        String[][] face = new String[5][5];
         JackOLantern face1 = new JackOLantern(face);
 
         face1.fill("X");
-        face1.edit("0", 1, 2);
-        face1.edit("0", 1, 5);
-        face1.edit("^", 2, 3);
-        face1.edit("^", 2, 4);
-        face1.edit("|", 3, 1);
-        face1.edit("|", 3, 2);
+        face1.edit("0", 2, 2);
+        face1.edit("0", 2, 4);
+        face1.edit("<", 3, 3);
+        face1.edit("(", 4, 2);
+        face1.edit(")", 4, 4);
         face1.edit("-", 3, 3);
-        face1.edit("-", 3, 4);
-        face1.edit("|", 3, 5);
-        face1.edit("|", 3, 6);
         System.out.println(face1);
+    }
 }
